c228c4b8213ee9b31a72976a536ffcba94af2843
j0r15 <joris@baldr.eu>
2019-09-09 17:04:30 +0200

    password updgrade of lib

--- PorLaGDiosfullversion/src/main.cpp
+++ PorLaGDiosfullversion/src/main.cpp
@@ -1,28 +1,24 @@
 #include <Arduino.h>
-#include <Keypad.h>
+#include "Keypad.h"
+#include "Password.h"
 #include <EEPROM.h>
 #define MAX_NUM_CHARS 8
-char customKey;
+char command;
 char cmd[MAX_NUM_CHARS];
 boolean cmd_complete = false;
 int addr = 0;
 byte value;
-int greenPin = 51;
-int redPin = 53;
+int greenLED = 51;
+int redLED = 53;
 
 long previousMillis = 0;        // will store last time LED was updated
 long interval = 1000;           // interval at which to blink (milliseconds)
 
-#define Password_Length 5
-char Data[Password_Length];
-char Master[Password_Length] = "1235";
-byte data_count = 0, master_count = 0;
-bool Pass_is_good;
-
+Password password = Password( "4155" );
 const byte ROWS = 4;
 const byte COLS = 3;
 
-char hexaKeys[ROWS][COLS] = {
+char keys[ROWS][COLS] = {
 
   {'1', '2', '3'},
   {'4', '5', '6'},
@@ -61,6 +57,10 @@ that means that mode work till the speed around 24
 bc if value is 249 is okay
 but if value is 259 is not okay :(
 
+
+speed < 25 ; mode 1-4
+speed > 25 ; mode
+
 lamp  speed  mode
 00    10     5
 01    10     5
@@ -83,7 +83,7 @@ void printUsage() {
   Serial.println((const __FlashStringHelper *)usageText);
 }
 
-void save(void){ //updates addr with values 0-255
+void save(void) { //updates addr with values 0-255
     EEPROM.update(addr, value);
     addr = addr + 1;
     if (addr == 16) {
@@ -112,15 +112,7 @@ void read(void) { //reads everything from EEPROM
 }
 
 
-void clearData(){ //resets string of keypad input
-  while(data_count !=0){
-    cmd[data_count--] = 0;
-    Data[data_count--] = 0;
-  }
-  return;
-}
-
-void process_command() { //handler for input
+void process_command(void) { //handler for input
   String readString2 = cmd;
   readString2.replace("#","");
   String readString = readString2;
@@ -145,7 +137,6 @@ void process_command() { //handler for input
     int lamp0  = atoi(lampbegin.c_str());
     int lamp1  = atoi(lampend.c_str());
     for (int lamp = lamp0 ; lamp < lamp1; lamp++){
-      // cout << "addressed lamp: " << lamp << endl;
       Serial.print("lamp: ");
       Serial.println(lamp);
       Serial.print("speed: ");
@@ -154,80 +145,111 @@ void process_command() { //handler for input
   }
 }
 
-void setup(){
-  Serial.begin(9600);
-  printUsage();
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  //printlastsaved();
-  //CHECK String readString = "##";
-  read(); // should load previous config
-  Serial.print("Enter Password:\n");
+
+
+void displayCodeEntryScreen()
+{
+  password.reset();
+  Serial.println("Enter Code:");
+  keypad.addEventListener(keypadEvent); //add an event listener for this keypad
+  //setup and turn off both LEDs
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, LOW);
 }
+
 
 void green(int wait) {
   digitalWrite(greenPin, HIGH);
   delay(wait);
   digitalWrite(greenPin, LOW);
   delay(wait);
-
 }
 
-void red(int wait){
+void unlockdoor(){ //controls servo that locks the door
+  // myservo.write(90);
+  digitalWrite(greenLED, HIGH);
+  delay(5000);
+}
+
+void red(int wait) {
   digitalWrite(redPin, HIGH);
   delay(wait);
   digitalWrite(redPin, LOW);
   delay(wait);
-
 }
-//----------------------------------------------------------------------------------
-void passwordCheck(void){
-    customKey = customKeypad.getKey();
-    if (customKey){
-      Data[data_count] = customKey;
-      data_count++;
-    }
 
-    if(data_count == Password_Length-1){
-      if(!strcmp(Data, Master)){
-        green(50); // flash green LED 3 times to confirm password
-        green(50);
-        green(50);
-        Serial.print("Correct");
-        //------------------------------------------------------------------------------Correct password
-        Pass_is_good = true;
-        clearData();
-        }
-
-      else {
-        red(50); // flash red LED 3 times to deny password
-        red(50); // also indicator that password is needed
-        red(50);
-        Serial.print("incorrect");
-        //------------------------------------------------------------------------------Incorrect passwords
-        Pass_is_good = false;
-        clearData();
-        }
+void checkPassword(){
+  if (password.evaluate()){
+    digitalWrite(greenLED, HIGH);
+    delay(30);
+    // lcd.print("Acces Granted");
+    // lcd.print("Welcome");
+    unlockdoor();
+    delay(2500);
+    displayCodeEntryScreen();
+  }
+  else{
+    loop(); {
+      red(50);
+      red(50);
+      red(50);
     }
+    delay(10);
+    // lcd.print("Acces Denied");
+    delay(2500);
+    displayCodeEntryScreen();
+  }
 }
-//----------------------------------------------------------------------------------
 
-void loop() {
-  passwordCheck();
-  customKey = customKeypad.getKey();
-  if (Pass_is_good = true) {
-    if (customKey) {
-      cmd[data_count] = customKey;
-      data_count++;
+//take care of some special events
+void keypadEvent(KeypadEvent eKey){
+  switch (keypad.getState()){
+  case PRESSED:
+    // lcd.print(eKey);
+    switch (eKey){
+    case '#':
+      checkPassword();
+      break;
+    case '*':
+         displayCodeEntryScreen(); break;
+    default:
+      password.append(eKey);
     }
-    if (customKey) {
-      Serial.print(customKey); //print input
-    }
-    if(data_count == MAX_NUM_CHARS || customKey == '#') {
-      // Serial.println(cmd);
-      process_command();
-      clearData();
+ switch (keypad.getState()){
+   case PRESSED:
+   switch (eKey){
+   case 'D': displayCodeEntryScreen();
     }
   }
+  }
+}
+
+
+
+
+//----------SETUP-&-LOOP-----------------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  keypad.setDebounceTime(50);
+  keypad.addEventListener(keypadEvent); //add an event listener for this keypad
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  digitalWrite(redLED, LOW);
+  digitalWrite(greenLED, LOW);
+
+  printUsage();
+  read(); // should load previous config
+}
+
+void loop() {
+  keypad.getKey();
+  command = customKeypad.getKey();
+
+  // if(data_count == MAX_NUM_CHARS || command == '#') {
+  //   // Serial.println(cmd);
+  //   process_command();
+  //   clearData();
+  // }
 }
